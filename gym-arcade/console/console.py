5fe0b2cf38a403791b2758d779bb61f93f12cf82
christian nickerson <christian_nickerson@hotmail.com>
2022-12-20 23:48:44 +0000

    updated pyproject and deps

--- gym-arcade/console/console.py
+++ gym-arcade/console/console.py
@@ -1,18 +1,23 @@
 from subprocess import call
 from pathlib import Path
 import retro
+import time
 
 
 class ArcadeConsole:
 
     """Console for running games. Requires controllers to play."""
 
-    def __init__(self, rom_directory: str) -> None:
+    def __init__(self, game_name: str, rom_directory: str = None) -> None:
         """ArcadeConsole initialiser.
 
-        :param rom_directory: directory to import ROM files from
+        :param game_name: name of game to start console with.
+        :param rom_directory: directory to import ROM files from, defaults to None.
         """
-        self._import_roms(Path(rom_directory))
+        if not rom_directory:
+            self._import_roms(Path(rom_directory))
+        self.env = retro.make(game=game_name)
+        self.env.reset()
 
     @staticmethod
     def _import_roms(rom_directory: Path) -> None:
